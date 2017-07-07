d95081584bd56ca7aa174e785d04c8e3e724c557
coleworsley <cole.s.worsley@gmail.com>
2017-07-07 13:54:50 -0600

    add fetch mock test

--- src/Components/Sidebar/Sidebar.test.js
+++ src/Components/Sidebar/Sidebar.test.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import { shallow, mount } from 'enzyme'
+import { filmMock } from '../../Helpers/MockData/filmMock'
 import fetchMock from 'fetch-mock'
 
 describe('SIDEBAR TEST - ALL', () => {
+  const resolveAfter2Seconds = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
+  }
+
+
+
 
   afterEach(() => {
     expect(fetchMock.calls().unmatched).toEqual([]);
     fetchMock.restore()
-})
-
-const stub = {count: 1,
-  results: [{episode_id: 4, opening_crawl: 'TEST TEST TEST', title: 'TEST TITLE TEST'}]
-}
-
-  it('submits the correct data to the server', async () => {
-    fetchMock.post('https://swapi.co/api/films', {
-      status: 200,
-      body: stub,
-    })
-    const wrapper = mount(<Sidebar/>);
-    await wrapper.update()
   })
+
+  it('should display an error when it doesnt get the stuff', async () => {
+    fetchMock.get('https://swapi.co/api/films', {
+      status: 500,
+      body: filmMock,
+    });
+
+    const wrapper = mount(<Sidebar/>);
+    await resolveAfter2Seconds();
+
+    expect(fetchMock.called()).toEqual(true);
+    expect(wrapper.state().films.allFilms.length).toEqual(2);
+  });
+
+
+
+
+
+
+
+
+
 
   // console.log(wrapper.debug())
 
