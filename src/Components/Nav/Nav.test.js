0654eb2b28b898135137193204e991d02faa961a
coleworsley <cole.s.worsley@gmail.com>
2017-07-08 11:25:29 -0600

    add nav tests

--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -4,10 +4,20 @@ import { shallow, mount } from 'enzyme'
 
 describe('NAV - ALL', () => {
   it('should contain 4 buttons', () =>{
+    const wrapper = shallow(<Nav/>)
+    const buttons = wrapper.find('button');
 
+    expect(buttons.length).toEqual(4);
   })
 
   it('should fire an onclick function when any of the 4 buttons are clicked', () =>{
+    const mockfn = jest.fn();
+    const activeCard = 'people';
+    const wrapper = shallow(<Nav getData={mockfn} activeCard={activeCard}/>);
 
+    const vehiclesBtn = wrapper.find('.vehicles-button')
+    console.log(vehiclesBtn.debug())
+
+    expect(true).toEqual(false);
   })
 });
